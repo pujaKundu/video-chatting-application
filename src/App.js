ddbcfbcf3877841b4bfb1878f2ea4dd24c0f40a6
pujaKundu <iampujakundu@gmail.com>
2023-05-15 19:38:38 +0600

    logics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Notifications from "./components/Notifications";
+import Options from "./components/Options";
+import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="font-semibold text-4xl text-blue-900">Video Chat App</h1>
+      <VideoPlayer />
+
+      <Options>
+        <Notifications />
+      </Options>
     </div>
   );
 }
